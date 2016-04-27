b45751d9412ea3196cab9bf63b8000c19ca8b4f8
David Rubio <drbotro@hotmail.com>
2016-04-27 18:50:28 +0200

    Video6 mod. app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var path = require('path');
 // Initializing express application
 var app = express();
 
-// Loading Config
+// Loading config
 var config = require('./lib/config');
 
 // Body Parser
@@ -23,9 +23,7 @@ app.use(logger('dev'));
 
 // Cookies / Session
 var cookieParser = require('cookie-parser');
-var session = require('./lib/helpers/session');
 app.use(cookieParser());
-app.use(session);
 
 // Layout setup
 var exphbs = require('express-handlebars');
@@ -48,7 +46,7 @@ if (!config().html.css.stylusPrecompile) {
     );
 }
 
-// Handlebars setup
+// Handlerbars setup
 app.engine(config().views.engine, exphbs({
     extname: config().views.extension,
     defaultLayout: config().views.layout,
@@ -61,8 +59,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', config().views.engine);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Sending config to templates
-app.use(function(req, res, next) {
+// Sending config to templeates
+app.use(function (req, res, next) {
     res.locals.config = config();
     next();
 });
@@ -72,9 +70,9 @@ app.disable('x-powered-by');
 
 require('./router')(app);
 
-// Export application or start the server
+// Export the application or start the server
 if (!!module.parent) {
     module.exports = app;
-} else {
+}else {
     app.listen(config().serverPort);
 }
